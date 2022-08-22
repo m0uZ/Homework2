9745489c8a281ab0576dea3c03baf95930615aa4
m0uZ <m.fraltsov@gmail.com>
2022-08-23 00:20:39 +0300

    Решение задачи №10

---
+++ task010/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите трехзначное и нажмите клавишу Enter: ");
+
+int a = int.Parse(Console.ReadLine());
+int b = 0;
+
+
+if (99 < a & a < 1000 )
+{
+    b = a / 10 % 10;
+    Console.WriteLine(b);
+}
+else 
+{
+    Console.WriteLine("Введено не трехзначное число!");
+}
