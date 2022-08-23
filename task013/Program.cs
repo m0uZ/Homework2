e7a4a949429939251a2bdeb2ac29422b1d6caddd
m0uZ <m.fraltsov@gmail.com>
2022-08-23 22:46:54 +0300

    Решение задачи №13

---
+++ task013/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.Write("Введите и нажмите клавишу Enter: ");
+
+string n_str = Console.ReadLine();
+int len = n_str.Length;
+int n = Convert.ToInt32(n_str);
+int i = 0;
+
+if (len <=2)
+{
+    Console.WriteLine("Третьей цифры нет!");
+}
+else
+{
+    while (i < (len - 3))
+
+{
+    n = n / 10;
+    i++;
+}
+Console.WriteLine(n % 10);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+{
+    n = n / 10;
+    i++;
+}
+Console.WriteLine(n % 10);
+*/
