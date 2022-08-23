c6079dcea0be80632023a059d7e268941a68e04c
m0uZ <m.fraltsov@gmail.com>
2022-08-24 01:07:51 +0300

    Решение задачи 15

---
+++ task015/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число и нажмите клавишу Enter: ");
+
+int a = int.Parse(Console.ReadLine());
+
+if (a > 5 & a < 8)
+{
+    Console.WriteLine("Chill");
+}
+else
+{
+    Console.WriteLine("Work");
+}
