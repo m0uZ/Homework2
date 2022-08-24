3082c1102b449e5da0a930aafe3120cb730ced39
m0uZ <m.fraltsov@gmail.com>
2022-08-24 18:42:25 +0300

    Решение дополнительной задачи

---
+++ optional_task/Program.cs
@@ -0,0 +1,23 @@
+﻿int a = Convert.ToInt32(Console.ReadLine());
+int b = a % 10;
+int c = a % 100;
+if (b == 1 & c != 11)
+{
+    Console.WriteLine(a + " программист");
+}
+else if (b == 2 & c != 12)
+{
+    Console.WriteLine(a + " программиста");
+}
+else if (b == 3 & c != 13)
+{
+    Console.WriteLine(a + " программиста");
+}
+else if (b == 4 & c != 14)
+{
+    Console.WriteLine(a + " программиста");
+}
+else
+{
+    Console.WriteLine(a + " программистов");
+}
